ba7120c30311c3091c3d1aee226b7fedd4c280eb
MiyuruCG <miyurugunatilake@gmail.com>
2020-09-13 19:31:04 +0530

    Start learning Fortran

---
+++ fotron/helloWorld.f
@@ -0,0 +1,13 @@
+
+	program helloworld
+		implicit none
+		!print*, "Hello World"
+		real :: a, b, res
+		
+		a = 15
+		b = 17
+		res = a + b
+
+		print *, "Total is",res
+
+	end program helloworld
---
+++ fotron/variables.f
@@ -0,0 +1,9 @@
+
+      program testingVariables
+    
+        ! print*, "Helllo"
+        integer :: largeval
+
+        print*, huge(largeval)
+    
+      end program testingVariables
