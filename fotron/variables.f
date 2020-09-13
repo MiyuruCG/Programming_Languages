fd32fc4cd0f26e3147cd037f2a0ce2be5081dfa9
MiyuruCG <miyurugunatilake@gmail.com>
2020-09-13 19:57:35 +0530

    'real' type variables

--- fotron/variables.f
+++ fotron/variables.f
@@ -1,9 +1,17 @@
+!gfortran
 
       program testingVariables
     
-        ! print*, "Helllo"
+        !print*, "Helllo"
+        !integer
         integer :: largeval
+        !print*, huge(largeval)
 
-        print*, huge(largeval)
+        !real
+        real :: p, q, res
+        p = 2.0
+        q = 3.0
+        res = p/q
+        print*, res
     
       end program testingVariables
