59ac431527ea25936b8be93cd185d678d867ad1b
MiyuruCG <miyurugunatilake@gmail.com>
2020-09-13 19:59:54 +0530

    'character' type variables

--- fotron/variables.f
+++ fotron/variables.f
@@ -1,17 +1,24 @@
 !gfortran
 
       program testingVariables
-    
+        implicit none
         !print*, "Helllo"
-        !integer
-        integer :: largeval
+
+        ! ----- integer
+        !integer :: largeval
         !print*, huge(largeval)
 
-        !real
-        real :: p, q, res
-        p = 2.0
-        q = 3.0
-        res = p/q
-        print*, res
+        ! ---- real
+        ! real :: p, q, res
+        ! p = 2.0
+        ! q = 3.0
+        ! res = p/q
+        ! print*, res
+
+        ! ---- character 
+        character (len = 40 ) :: name
+        name = "Miyuru CG"
+        print*, name
+        
     
       end program testingVariables
